fce1b581971be306cda73c077b946e5507be6bc9
Ubuntu <ubuntu@ip-172-31-18-62.ec2.internal>
2021-04-28 19:07:44 +0000

    added in basicMath.cpp and areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Year Jonathan Kene All rights reserved.
+//
+// Created by: Jonathan Kene
+// Created on: Apr. 28, 2021
+// This program calculates and displays the area and
+// the perimeter of a rectangle with dimensions 8cm X 6 cm
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 8cm and width 6cm\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 8*6 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (8+6) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) Year Jonathan Kene All rights reserved.
+//
+// Created by: Jonathan Kene
+// Created on: Apr. 28, 2021
+// This program does basic math.
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << "5 + 3 = " << 5+3 << std::endl;
+  std::cout << "9 - 8 = " << 9-8 << std::endl;
+  std::cout << "144 / 12 = " << 144/12 << std::endl;
+  std::cout << "16 - 8 / 2 = " << 16-8/2 << std::endl;
+  std::cout << "2^3 = " << pow(2, 3) << std::endl;
+  std::cout << "√81 = " << sqrt(81) << std::endl;
+  std::cout << "3 / 4 = " << float(3)/float(4) << std::endl;
+}
